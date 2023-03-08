0d46fb1a3df18c4152c09ac2c4feb08a39dc2153
joannec34 <joannechen9058@gmail.com>
2023-03-08 14:01:58 -0500

    vue

---
+++ vue-reactivity/src/mainVue.vue
@@ -0,0 +1,23 @@
+<template>
+  <secondVue msg="hello" />
+  <secondVue msg="goodbye" />
+</template>
+
+<script>
+import secondVue from "./components/secondVue.vue";
+
+export default {
+  name: "mainVue",
+  components: {
+    secondVue,
+  },
+  data() {
+    return {
+      people:"ur mom", "ur dad",
+      places: "house"
+    };
+  },
+};
+</script>
+
+<style></style>
