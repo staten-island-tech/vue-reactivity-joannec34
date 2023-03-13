28ff5b996122f06268be62f089d657dfee495417
joannec34 <joannechen9058@gmail.com>
2023-03-13 14:00:18 -0400

    djkadl

--- vue-reactivity/src/components/cardVue.vue
+++ vue-reactivity/src/components/cardVue.vue
@@ -1,27 +1,35 @@
 <template>
+  <div>
     <h1>{{ title }}</h1>
     <h2 v-if="graduated">{{ student }}</h2>
     <h2 v-else>{{ student }} did not graduate</h2>
     <ul>
       <li v-for="animal in animals" :key="animal">{{ animal }}</li>
     </ul>
-  </template>
-  
-  <script>
-  export default {
-    name: "cardVue",
-    props: {
-      title: String,
-    },
-    data() {
-      return {
-        student: "cynthia",
-        graduated: false,
-        animals: ["cat", "dog", "rat"],
-      };
-    },
-  };
-  </script>
-  
-  <style scoped></style>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cardVue",
+  props: {
+    title: String,
+    student: String,
+  },
+  data() {
+    return {
+      //students: ["cynthia", "natalie"],
+      graduated: false,
+      animals: ["cat", "dog", "rat"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: aqua;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
--- vue-reactivity/src/mainVue.vue
+++ vue-reactivity/src/mainVue.vue
@@ -1,6 +1,10 @@
 <template>
   <headerVue title="hello" />
-  <cardVue title="animals" />
+  <div class="cards">
+    <cardVue title="animals" student="cynthia" />
+    <cardVue title="animals2" student="natalie" />
+  </div>
+
   <button @click="authState" v-if="loggedIn">log out</button>
   <button @click="authState" v-else>log in</button>
 </template>
@@ -31,4 +35,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.cards {
+  display: flex;
+  justify-content: center;
+}
+</style>
